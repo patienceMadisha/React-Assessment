f3860658345d2da5a30ca218b933ea6538619c29
Lebo <lebomadisha28@gmail.com>
2022-06-02 09:51:04 +0200

    Assessment.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Carousel from "react-elastic-carousel";
+import Slider from "./Components/Slider/Slider";
+import crossBox from "./Components/Slider/Icons/box.png";
+import "./App.css";
+
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1920, itemsToShow: 5},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <div className="Container1">
+            <div className="Container2">
+                <div className="Text">
+                    what are you
+                    <br/> <strong> here to do? </strong>
+                </div>
+                <div className="App">
+                    <Carousel breakPoints={breakPoints}>
+                        <Slider>
+                                <img
+                                    className="Icon"
+                                    src={crossBox}
+                                />
+                                Mobile internet
+                        </Slider>
+                        <Slider>
+                            <img
+                                className="Icon"
+                                src={crossBox}
+                            />
+                            Home internet
+                        </Slider>
+                        <Slider>
+                            <img
+                                className="Icon"
+                                src={crossBox}
+                            />
+                            Get a device
+                        </Slider>
+                        <Slider>
+                            <img
+                                className="Icon"
+                                src={crossBox}
+                            />
+                            Add a phone-line
+                        </Slider>
+                        <Slider>
+                            <img
+                                className="Icon"
+                                src={crossBox}
+                            />
+                            Upgrade
+                        </Slider>
+                    </Carousel>
+                </div>
+            </div>
+        </div>
+        </>
+
+    );
 }
 
 export default App;
---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
+  border-radius: 20px;
+  width: 250px;
+  background-color: white;
+  margin-top: 100px
+  margin: 0 15px;
+  font-size: 1em;
+`;
